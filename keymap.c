d060c55ac22be7ac6afc2d6685f0f94a8e4578c9
dipaso50 <dipaso50@gmail.com>
2022-09-01 21:13:36 +0200

    Intellij magic

--- keymap.c
+++ keymap.c
@@ -12,12 +12,18 @@ enum layer_number {
 };
 
 enum {    
-    TD_ESCQ
+    TD_ESCQ,
+    TD_RENAME,
+    TD_ORG_IMPORTS,
+    TD_GENERATE,
 };
 
 
 qk_tap_dance_action_t tap_dance_actions[] = {
     [TD_ESCQ] = ACTION_TAP_DANCE_DOUBLE(KC_Q, KC_ESC),
+    [TD_RENAME] = ACTION_TAP_DANCE_DOUBLE(KC_R, LSFT(KC_F6)),
+    [TD_ORG_IMPORTS] = ACTION_TAP_DANCE_DOUBLE(KC_O, LALT(LCTL(KC_O))),
+    [TD_GENERATE] = ACTION_TAP_DANCE_DOUBLE(KC_G, LGUI(KC_N)),
 };
 
 #define KC_CAPW LGUI(LSFT(KC_3)) // Capture whole screen 
@@ -26,21 +32,24 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 #define KC_CPYP LGUI(LSFT(LCTL(KC_4))) // Copy portion of screen
 
 #define TD_Q_ESC TD(TD_ESCQ)
+#define TD_R_NAME TD(TD_RENAME) //rename variable or class in Intellij
+#define TD_IMPORTS TD(TD_ORG_IMPORTS) //organize imports in Intellj
+#define TD_GENERATE TD(TD_GENERATE)//generate code in IntelliJ
 
 #define TTT KC_TRNS
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {  
 
 [_QWERTY] = LAYOUT( 
-    TD_Q_ESC, KC_W, KC_E, KC_R, LT(_MULTIMEDIA,KC_T),                       KC_Y, KC_U, KC_I, KC_O, KC_P,     
-    KC_A, KC_S, KC_D, LT(_TILDE, KC_F), KC_G,                               KC_H, LT(_NUMBER, KC_J), KC_K, KC_L, KC_BSPC,    
+    TD_Q_ESC, KC_W, KC_E, TD_R_NAME, LT(_MULTIMEDIA,KC_T),                  KC_Y, KC_U, KC_I, TD_IMPORTS, KC_P,     
+    KC_A, KC_S, KC_D, LT(_TILDE, KC_F), TD_GENERATE,                               KC_H, LT(_NUMBER, KC_J), KC_K, KC_L, KC_BSPC,    
     LCTL_T(KC_Z), LGUI_T(KC_X), LALT_T(KC_C), LSFT_T(KC_V), KC_B,           KC_N , RSFT_T(KC_M), RALT_T(KC_COMM), RGUI_T(KC_DOT), LCTL_T(KC_SLSH),    
                                    KC_TAB , LT(_RAISE, KC_SPC), LT(_LOWER, KC_ENT), OSM(MOD_RGUI)),
 
 [_NUMBER] = LAYOUT(    
     KC_1, KC_2, KC_3, KC_4, KC_5,                                           TTT, TTT, TTT, TTT, TTT,    
     KC_6, KC_7, KC_8, KC_9, KC_0,                                           TTT, TTT,  TTT, TTT, TTT,    
-    ES_PLUS, ES_MINS, ES_ASTR, ES_SLSH, ES_EQL,                             TTT, TTT, TTT, TTT, TTT,     
+    ES_PLUS, ES_MINS, KC_COMM, KC_DOT, ES_EQL,                              TTT, TTT, TTT, TTT, TTT,     
                                         TTT, TTT, TTT, TTT),
     
 [_LOWER] = LAYOUT(    
