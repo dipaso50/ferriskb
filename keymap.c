4ebe83d22432534a529ce9dee2287b5eb2f1ac9e
dipaso50 <dipaso50@gmail.com>
2022-10-22 12:24:09 +0200

    cambio de home row mods

--- keymap.c
+++ keymap.c
@@ -45,9 +45,9 @@ qk_tap_dance_action_t tap_dance_actions[] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {  
 
 [_QWERTY] = LAYOUT( 
-    TD_Q_ESC, KC_W, KC_E, KC_R, LT(_MULTIMEDIA,KC_T),                       KC_Y, KC_U, KC_I, KC_O, KC_P,     
-    KC_A, KC_S, KC_D, LT(_TILDE, KC_F), KC_G,                               KC_H, LT(_NUMBER, KC_J), KC_K, KC_L, KC_BSPC,    
-    LCTL_T(KC_Z), LGUI_T(KC_X), LALT_T(KC_C), LSFT_T(KC_V), KC_B,           KC_N , RSFT_T(KC_M), RALT_T(KC_COMM), RGUI_T(KC_DOT), LCTL_T(KC_SLSH),    
+    TD_Q_ESC, KC_W, KC_E, KC_R, LT(_MULTIMEDIA,KC_T),                       KC_Y, KC_U, KC_I, KC_O, KC_P,   
+    LCTL_T(KC_A), LGUI_T(KC_S), LALT_T(KC_D), LSFT_T(KC_F), KC_G,           KC_H , RSFT_T(KC_J), RALT_T(KC_K), RGUI_T(KC_L), LCTL_T(KC_BSPC),   
+    KC_Z, KC_X, KC_C, LT(_TILDE,KC_V), KC_B,                                KC_N ,LT(_NUMBER,KC_M), KC_COMM, KC_DOT, KC_SLSH,    
                                    KC_TAB , LT(_RAISE, KC_SPC), LT(_LOWER, KC_ENT), OSM(MOD_RGUI)),
 
 [_NUMBER] = LAYOUT(    
